1e79b01c6ccf1e56ee601cd4f3df2729088af983
Haochen_Wang <whcfoxdanny@gmail.com>
2022-09-21 18:34:41 -0400

    141. Linked List Cycle
    141. Linked List Cycle

---
+++ 141. Linked List Cycle.cs
@@ -0,0 +1,105 @@
+141. Linked List Cycle
+//C#
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public bool HasCycle(ListNode head) {
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+		public bool HasCycle(ListNode head)
+            {
+                List <ListNode> list = new List<ListNode>() ;
+                while (head != null)
+                {
+                    if (list.Contains(head))
+                        return true;
+                    list.Add(head);
+                    head = head.next;
+                }
+                return false;
+            }
+
+//Java
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head, fast=head;
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow==fast){
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+//C++
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        unordered_set<ListNode*> s;
+        while (head != NULL) {        
+            if (s.find(head) != s.end())
+                return true; 
+            s.insert(head);
+ 
+            head = head->next;
+        }
+ 
+        return false;
+    }
+};
+bool hasCycle(ListNode *head)
+{
+    ListNode *slow_p = list, *fast_p = list;
+ 
+    while (slow_p && fast_p && fast_p->next) {
+        slow_p = slow_p->next;
+        fast_p = fast_p->next->next;
+        if (slow_p == fast_p) {
+            return true;
+        }
+    }
+    return false;
+}

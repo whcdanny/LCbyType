7c28b2d3c0ef3ccee1ded596e974db901c48de29
Haochen_Wang <whcfoxdanny@gmail.com>
2023-01-23 18:27:32 -0500

    Update 23. Merge k Sorted Lists.cs

--- ListNode/23. Merge k Sorted Lists.cs
+++ ListNode/23. Merge k Sorted Lists.cs
@@ -1,6 +1,7 @@
 //23:给一个ListNode的列表，要求合并所有并且按照升序； hard
 //c#暂时还没找到替换PriorityQueue 的办法，所以现在是Java
 //思路：把所以ListNode放入这个PriorityQueue 然后根据val来排序 
+//算法整体的时间复杂度是 O(Nlogk):其中 k 是链表的条数，N 是这些链表的节点总数
 	public ListNode mergeKLists(ListNode[] lists) {
         if (lists.length == 0) return null;       
         ListNode dummy = new ListNode(-1);

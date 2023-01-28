5aa8204eb1506ad3eb7c1b7b0e512d9602d5a54f
Haochen_Wang <whcfoxdanny@gmail.com>
2023-01-27 19:06:00 -0500

    Update 51. N-Queens.cs

--- 回溯算法/51. N-Queens.cs
+++ 回溯算法/51. N-Queens.cs
@@ -1,8 +1,9 @@
-//51. N-Queens hard
+//51 hard
 //给一个nxn棋盘，放入最多的Queens并且他们不能互相攻击
 //Queens的攻击是同一行，同一列，左上左下右上右下四个方向的任意单位；
 //'.' 表示空，'Q' 表示皇后，初始化空棋盘
-//思路：回溯算法 暴力算法 从[0,0]假设可以放Queen，然后如果根据Queen的位置条件都满足，那么就存下来，不满足就返回上一行
+//思路：回溯算法 暴力算法 从[0,0]假设可以放Queen，然后如果根据Queen的位置条件都满足，那么就存下来，不满足就返回上一行；
+//验证当前位置是否有用：检查列是否有皇后互相冲突；y=x 或者 y=-x 来表示检查右上左上是否有皇后互相冲突
 		public IList<IList<string>> SolveNQueens(int n)
         {
             var result = new List<IList<string>>();
@@ -45,13 +46,13 @@
             var n = rows.Count;
             for (var i = 0; i < n; i++)
             {
-				// 检查列是否有皇后互相冲突
+				//检查列是否有皇后互相冲突
                 var j = rows[i].IndexOf('Q');
                 if (j == col)
                 {
                     return false;
-                }
-				// 检查右上左上是否有皇后互相冲突
+                }				
+				//y=x 或者 y=-x 来表示检查右上左上是否有皇后互相冲突
                 if (Math.Abs(col - j) == Math.Abs(n - i))
                 {
                     return false;

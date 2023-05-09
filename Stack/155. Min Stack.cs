d4b441c79fd8027f5deba878220f2489333bc79d
Haochen_Wang <whcfoxdanny@gmail.com>
2023-05-09 13:40:12 -0400

    Stack
    Stack

---
+++ Stack/155. Min Stack.cs
@@ -0,0 +1,56 @@
+//155. Min Stack med
+//根据要求写一个Min Stack
+/*
+MinStack() initializes the stack object.
+void push(int val) pushes the element val onto the stack.
+void pop() removes the element on the top of the stack.
+int top() gets the top element of the stack.
+int getMin() retrieves the minimum element in the stack.*/
+/思路：给两个stack一个存每一个数，一个只存每个时刻的最小值，这样保证minStack的最上面总是当前最小的数；
+        public class MinStack
+        {
+
+            /** initialize your data structure here. */
+            Stack<int> stack = new Stack<int>();
+            Stack<int> minStack = new Stack<int>();
+
+            public MinStack()
+            {
+
+            }
+
+            public void Push(int x)
+            {
+                stack.Push(x);
+                if (minStack.Count == 0 || x <= GetMin())
+                    minStack.Push(x);
+            }
+
+            public void Pop()
+            {
+                if (stack.Count == 0)
+                    return;
+                int x = stack.Pop();
+                if (x == minStack.Peek())
+                    minStack.Pop();
+            }
+
+            public int Top()
+            {
+                return stack.Peek();
+            }
+
+            public int GetMin()
+            {
+                return minStack.Peek();
+            }
+        }
+
+        /**
+         * Your MinStack object will be instantiated and called as such:
+         * MinStack obj = new MinStack();
+         * obj.Push(x);
+         * obj.Pop();
+         * int param_3 = obj.Top();
+         * int param_4 = obj.GetMin();
+         */
---
+++ Stack/224. Basic Calculator.cs
@@ -0,0 +1,48 @@
+//224. Basic Calculator hard
+//给你一个字符串表达式 s ，请你实现一个基本计算器来计算并返回它的值
+//思路：在遍历字符串时，如果遇到数字，则将数字解析为整数添加到当前结果中。如果遇到加号或减号，则更新符号变量。如果遇到左括号，则将当前结果和符号入栈，并重新初始化结果和符号。如果遇到右括号，则从堆栈中弹出上一级结果和符号，并将当前结果乘以符号并加上上一级结果。最终结果为最后的结果
+        public int Calculate(string s)
+        {
+            int result = 0;
+            int sign = 1;
+
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                char c = s[i];
+
+                if (Char.IsDigit(c))
+                {
+                    int num = c - '0';
+                    while (i + 1 < s.Length && Char.IsDigit(s[i + 1]))
+                    {
+                        num = num * 10 + (s[i + 1] - '0');
+                        i++;
+                    }
+                    result += sign * num;
+                }
+                else if (c == '+')
+                {
+                    sign = 1;
+                }
+                else if (c == '-')
+                {
+                    sign = -1;
+                }
+                else if (c == '(')
+                {
+                    stack.Push(result);
+                    stack.Push(sign);
+                    result = 0;
+                    sign = 1;
+                }
+                else if (c == ')')
+                {
+                    result = stack.Pop() * result + stack.Pop();
+                    sign = 1;
+                }
+            }
+
+            return result;
+        }

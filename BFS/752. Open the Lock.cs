0f2c4acb6a8eaa407979ec473227b18a7446c031
Haochen_Wang <whcfoxdanny@gmail.com>
2023-10-06 13:49:56 -0400

    BFS
    BFS

--- BFS/752. Open the Lock.cs
+++ BFS/752. Open the Lock.cs
@@ -1,3 +1,46 @@
+//Leetcode 752. Open the Lock med
+//题意：题目要求通过旋转拨盘将锁打开。每个拨盘上都有10个数字（'0' 到 '9'）。开始时，锁的数字都是 '0'，你可以旋转拨盘将一个数字变成相邻的数字（例如 '0' 变成 '1'，'9' 变成 '0'），也可以将数字往回旋转。锁上有一个列表，其中包含四个代表目标数字的字符串，我们需要将锁从起始状态变为目标状态，每次只能旋转一个拨盘。。
+//思路：广度优先搜索（BFS）序列化： 将起始状态加入队列，同时初始化一个集合来记录已经访问过的状态。开始BFS，每次从队列中取出一个状态，将其拨盘旋转得到相邻状态，如果该状态已经访问过，则跳过。
+//时间复杂度: 字符串数组的长度为 n，拨盘上的数字个数为 m, 每个状态可以生成最多 8 个相邻状态O(8^n * m)
+//空间复杂度：O(8^n)
+        public int OpenLock(string[] deadends, string target)
+        {
+            HashSet<string> dead = new HashSet<string>(deadends);
+            HashSet<string> visited = new HashSet<string>();
+            Queue<string> queue = new Queue<string>();
+            string start = "0000";
+            if (dead.Contains(start)) return -1;
+            queue.Enqueue(start);
+            visited.Add(start);
+            int steps = 0;
+
+            while (queue.Count > 0)
+            {
+                int size = queue.Count;
+                for (int i = 0; i < size; i++)
+                {
+                    string current = queue.Dequeue();
+                    if (current == target) return steps;
+                    for (int j = 0; j < 4; j++)
+                    {
+                        for (int d = -1; d <= 1; d += 2)
+                        {
+                            int nextDigit = (current[j] - '0' + d + 10) % 10;
+                            string next = current.Substring(0, j) + nextDigit + current.Substring(j + 1);
+                            if (!dead.Contains(next) && !visited.Contains(next))
+                            {
+                                queue.Enqueue(next);
+                                visited.Add(next);
+                            }
+                        }
+                    }
+                }
+                steps++;
+            }
+
+            return -1;
+        }
+
 //752. Open the Lock med
 //有一个锁上面是0-9，每次旋转一次，给一个deadends 如果转到的数字包含在里面，就锁死无法转动，
 //target如果没找到，return-1；

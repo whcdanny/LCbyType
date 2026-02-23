c5e91cfc4a4cf837969a9880cf0a598d4a0e2cde
Haochen_Wang <whcfoxdanny@gmail.com>
2026-02-23 15:36:40 -0500

    2.23

---
+++ 2026/Monotonic Stack/907. Sum of Subarray Minimums.cs
@@ -0,0 +1,44 @@
+//Leetcode 907. Sum of Subarray Minimums med
+//题意：题目要求给定一个整数数组 arr，找到数组 arr 的所有子数组中的最小值，
+//然后计算这些最小值的总和，并对结果取模 10^9 + 7。
+//思路：Monotonic stack 单调栈
+//两个数组 left 和 right，其中 left[i] 表示以 arr[i] 为最小值的连续子数组的左边界，
+//right[i] 表示以 arr[i] 为最小值的连续子数组的右边界。
+//计算以 arr[i] 为最小值的子数组的个数，即 count = (i - left[i]) * (right[i] - i)。
+//将 arr[i] 乘以 count 加入结果中。
+//最后返回结果对 10^9 + 7 取模的值。
+//时间复杂度: O(n)，其中 n 是数组 arr 的长度
+//空间复杂度：O(n)。
+        public int SumSubarrayMins(int[] arr)
+        {
+            int MOD = (int)Math.Pow(10, 9) + 7;
+            int n = arr.Length;
+            long sum = 0;
+            int[] left = new int[n];
+            int[] right = new int[n];
+            Stack<int> stack = new Stack<int>();
+            for (int i = 0; i < n; i++)
+            {
+                while (stack.Count > 0 && arr[stack.Peek()] >= arr[i])
+                {
+                    stack.Pop();
+                }
+                left[i] = stack.Count > 0 ? stack.Peek() : -1;
+                stack.Push(i);
+            }
+            stack.Clear();
+            for (int i = n - 1; i >= 0; i--)
+            {
+                while (stack.Count > 0 && arr[stack.Peek()] > arr[i])
+                {
+                    stack.Pop();
+                }
+                right[i] = stack.Count > 0 ? stack.Peek() : n;
+                stack.Push(i);
+            }
+            for (int i = 0; i < n; i++)
+            {
+                sum = (sum + (long)arr[i] * (i - left[i]) * (right[i] - i)) % MOD;
+            }
+            return (int)sum;
+        }
